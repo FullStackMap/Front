d988e5796ae9e778054ebf3eb79802e44b37688b
Dercraker <antoine.capitain@gmail.com>
2024-02-15 14:22:04 +0100

    wa des variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./styles.css";
-import { DoubleNavbar } from "./DoubleSearch";
 import '@mantine/core/styles.css';
+import Footer from "./component/layout/footer";
 
 export default function App() {
   return (
     <div className="App">
-      <DoubleNavbar />
+      <Footer />
     </div>
   );
 }
