51146d07fdecd77923566dbc0b96618c45c13e2b
Dercraker <antoine.capitain@gmail.com>
2024-03-01 23:37:00 +0100

    Fix (Client Api) : Implémentation

--- src/services/BaseApi.ts
+++ src/services/BaseApi.ts
@@ -1,25 +1,24 @@
 import * as client from '@FullStackMap/from-a2b';
 import { AuthStore, useAuthStore } from '../store/useAuthStore';
 
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const token: string | undefined = useAuthStore(
-  (state: AuthStore) => state.token,
-);
-
-const configLogged = new client.Configuration({
-  basePath: '/server',
-  baseOptions: {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  },
-});
-
 const configAno = new client.Configuration({
   basePath: '/server',
 });
 
-export const TripController = client.TripApiFactory(configLogged);
+export const TripController = () => {
+  const token: string | undefined = useAuthStore(
+    (state: AuthStore) => state.token,
+  );
+
+  const configLogged = new client.Configuration({
+    basePath: '/server',
+    baseOptions: {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  });
+  return client.TripApiFactory(configLogged);
+};
 
 export const AnoAuthController = client.AuthApiFactory(configAno);
-export const AnoTripController = client.TripApiFactory(configAno);
