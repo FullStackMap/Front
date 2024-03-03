533fa8828d343b329d77f1ff2c2084940c205d07
Baptiste Legat <legatbaptiste@gmail.com>
2024-03-03 16:05:17 +0100

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import { createTheme, MantineProvider } from '@mantine/core';
 
 const theme = createTheme({
-	/** Put your mantine theme override here */
+  fontFamily: 'Poppins, sans-serif',
+  primaryColor: 'teal',
+  
 });
 
 const rootElement = document.getElementById('root')!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<MantineProvider theme={theme}>
-				<App />
-			</MantineProvider>
-		</BrowserRouter>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <MantineProvider theme={theme}>
+        <App />
+      </MantineProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
--- src/pages/landing/Content.tsx
+++ src/pages/landing/Content.tsx
@@ -1,92 +1,118 @@
 import {
-  Box,
+  Container,
   Text,
   SimpleGrid,
   Card,
   Group,
   Timeline,
   TimelineItem,
-  Center,
   Paper,
   Title,
+  Badge,
 } from '@mantine/core';
 import {
   IconPhoto,
   IconMapPin,
   IconCalendar,
   IconCheck,
+  IconGlobe,
+  IconMap,
+  IconCompass,
+  IconTicket,
+  IconDownload,
+  IconTimeline,
 } from '@tabler/icons-react';
 
 export function Content() {
   return (
-    <Box>
-      <Box mt={50}>
-        <Center>
-          <Timeline color="gray" active={1} lineWidth={6} bulletSize={40}>
-            <TimelineItem bullet={<IconPhoto size={18} />}>
-              <Paper shadow="xl" withBorder radius="lg" p="md">
-                <Title order={4}>
-                  Trouvez des destinations pour votre prochain voyage
-                </Title>
-                <Text>
-                  Utilisez notre moteur de recherche pour trouver des
-                  destinations en fonction de vos préférences.
-                </Text>
-              </Paper>
-            </TimelineItem>
-            <TimelineItem bullet={<IconMapPin size={18} />}>
-              <Paper shadow="xl" withBorder radius="lg" p="md">
-                <Title order={4}>
-                  Créez votre itinéraire et partagez-le avec vos amis
-                </Title>
-                <Text>
-                  Utilisez notre outil de planification pour créer votre
-                  itinéraire et partagez-le avec vos amis pour obtenir leur
-                  avis.
-                </Text>
-              </Paper>
-            </TimelineItem>
-            <TimelineItem bullet={<IconCalendar size={18} />}>
-              <Paper shadow="xl" withBorder radius="lg" p="md">
-                <Title order={4}>
-                  Réservez vos billets d'avion, de train ou de bus
-                </Title>
-                <Text>
-                  Utilisez notre moteur de recherche pour trouver les meilleurs
-                  tarifs pour vos billets d'avion, de train ou de bus.
-                </Text>
-              </Paper>
-            </TimelineItem>
-            <TimelineItem bullet={<IconCheck size={18} />}>
-              <Paper shadow="xl" withBorder radius="lg" p="md">
-                <Title order={4}>Profitez de votre voyage</Title>
-                <Text>
-                  Suivez votre itinéraire et profitez de votre voyage en toute
-                  sérénité grâce à notre application.
-                </Text>
-              </Paper>
-            </TimelineItem>
-          </Timeline>
-        </Center>
-      </Box>
-      <SimpleGrid mt={20} mx={40} cols={3} spacing="lg">
+    <Container size="lg" py="xl" mt={50} ta="center">
+      <Badge variant="filled" size="xl" mb={50}>
+        Organisez votre prochain voyage en toute sérénité
+      </Badge>
+      <Timeline color="gray" active={1} lineWidth={6} bulletSize={40}>
+        <TimelineItem bullet={<IconPhoto size={18} />}>
+          <Paper shadow="xl" withBorder radius="lg" p="md">
+            <Title order={4}>
+              Trouvez des destinations pour votre prochain voyage
+            </Title>
+            <Text size="sm" c="teal.8">
+              Utilisez notre moteur de recherche pour trouver des destinations
+              en fonction de vos préférences.
+            </Text>
+          </Paper>
+        </TimelineItem>
+        <TimelineItem bullet={<IconMapPin size={18} />}>
+          <Paper shadow="xl" withBorder radius="lg" p="md">
+            <Title order={4}>
+              Créez votre itinéraire et partagez-le avec vos amis
+            </Title>
+            <Text size="sm" c="teal.8">
+              Utilisez notre outil de planification pour créer votre itinéraire
+              et partagez-le avec vos amis pour obtenir leur avis.
+            </Text>
+          </Paper>
+        </TimelineItem>
+        <TimelineItem bullet={<IconCalendar size={18} />}>
+          <Paper shadow="xl" withBorder radius="lg" p="md">
+            <Title order={4}>
+              Réservez vos billets d'avion, de train ou de bus
+            </Title>
+            <Text size="sm" c="teal.8">
+              Utilisez notre moteur de recherche pour trouver les meilleurs
+              tarifs pour vos billets d'avion, de train ou de bus.
+            </Text>
+          </Paper>
+        </TimelineItem>
+        <TimelineItem bullet={<IconCheck size={18} />}>
+          <Paper shadow="xl" withBorder radius="lg" p="md">
+            <Title order={4}>Profitez de votre voyage</Title>
+            <Text size="sm" c="teal.8">
+              Suivez votre itinéraire et profitez de votre voyage en toute
+              sérénité grâce à notre application.
+            </Text>
+          </Paper>
+        </TimelineItem>
+      </Timeline>
+      <Badge variant="filled" size="xl" mt={80}>
+        Découvrez nos fonctionnalités
+      </Badge>
+      <Text c="dimmed" mt="md">
+        Découvrez les fonctionnalités de notre application pour organiser votre
+        prochain voyage.
+      </Text>
+      <SimpleGrid mt={20} cols={3} spacing="lg">
         <DestinationCard
-          icon={<IconPhoto size={24} />}
+          icon={<IconGlobe color="teal" size={24} />}
           title="Un voyage sur mesure"
           description="Trouvez des destinations pour votre prochain voyage"
         />
         <DestinationCard
-          icon={<IconMapPin size={24} />}
+          icon={<IconMap color="teal" size={24} />}
           title="Planifiez votre itinéraire"
-          description="Créez votre itinéraire et partagez-le avec vos amis"
+          description="Créez votre itinéraire de bout en bout"
         />
         <DestinationCard
-          icon={<IconCalendar size={24} />}
+          icon={<IconCompass color="teal" size={24} />}
+          title="Proposition d'activités"
+          description="Recevez des propositions d'activités en fonction de vos préférences"
+        />
+        <DestinationCard
+          icon={<IconTimeline color="teal" size={24} />}
+          title="Calcul de l'estimation du temps"
+          description="Calculez l'estimation du temps pour chaque activité"
+        />
+        <DestinationCard
+          icon={<IconTicket color="teal" size={24} />}
           title="Réservez vos billets"
           description="Réservez vos billets d'avion, de train ou de bus"
         />
+        <DestinationCard
+          icon={<IconDownload color="teal" size={24} />}
+          title="Téléchargez votre itinéraire"
+          description="Téléchargez votre itinéraire pour l'utiliser hors ligne"
+        />
       </SimpleGrid>
-    </Box>
+    </Container>
   );
 }
 
@@ -103,9 +129,9 @@ const DestinationCard = ({
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Group mb="xs">
         {icon}
-        <Text w={500}>{title}</Text>
+        <Text>{title}</Text>
       </Group>
-      <Text size="sm" c="dimmed">
+      <Text size="sm" c="dimmed" color="">
         {description}
       </Text>
     </Card>
