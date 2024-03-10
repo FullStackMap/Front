2b8e82908dd9d90995ff717fdb724049a8f71466
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2024-03-10 21:08:31 +0100

    add currentPath for knowing the value of the Tabs

--- src/components/tabTravels/TabTravels.tsx
+++ src/components/tabTravels/TabTravels.tsx
@@ -1,18 +1,29 @@
 import { Tabs } from '@mantine/core';
-import { useNavigate } from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 
 const TabTravels = () => {
+
   const navigate = useNavigate();
+
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const getPageName = (path: string) => {
+    const modifiedPath = path.replace('/trips', '');
+    if (modifiedPath === '') return 'home'
+    return modifiedPath;
+  };
+
   return (
-    <Tabs defaultValue="first">
+    <Tabs value={getPageName(currentPath)}>
       <Tabs.List>
-        <Tabs.Tab value="first" onClick={() => navigate('')}>
+        <Tabs.Tab value="home" onClick={() => navigate('')}>
           Mes Voyages
         </Tabs.Tab>
-        <Tabs.Tab value="third" onClick={() => navigate('history')}>
+        <Tabs.Tab value="/history" onClick={() => navigate('history')}>
           Historique
         </Tabs.Tab>
-        <Tabs.Tab value="second" onClick={() => navigate('statistics')}>
+        <Tabs.Tab value="/statistics" onClick={() => navigate('statistics')}>
           Statistiques
         </Tabs.Tab>
       </Tabs.List>
