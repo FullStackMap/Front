712e2c6cdc651a9a4f5e305700adc882cd246538
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2024-03-10 22:16:23 +0100

    show-travels update MyTrips.tsx add if query is null take only is same or after the actual date

--- src/pages/showTravels/myTrips/MyTrips.tsx
+++ src/pages/showTravels/myTrips/MyTrips.tsx
@@ -1,9 +1,12 @@
-// import {useQuery} from '@tanstack/react-query'
-
-import { TripDto } from '@FullStackMap/from-a2b';
 import { useQuery } from '@tanstack/react-query';
 import { TripController } from '../../../services/BaseApi';
 import { AuthStore, useAuthStore } from '../../../store/useAuthStore';
+
+import NoFoundTravels from "../../../components/cardCustom/noFoundTravels/NoFoundTravels.tsx";
+import {CurrentTravelCard} from "../../../components/cardCustom/currentTravelCard/CurrentTravelCard.tsx";
+
+import moment from 'moment';
+
 
 const MyTrips = () => {
   const userId = useAuthStore((s: AuthStore) => s.user?.Id);
@@ -23,13 +26,27 @@ const MyTrips = () => {
 
   if (isError) return <div>Error</div>;
 
+  if (!Array.isArray(query)) return <NoFoundTravels />;
+
+  const currentDate = moment();
+
+  const trips = query.filter(trip => {
+    const tripStartDate = moment(trip.endDate);
+    return tripStartDate.isSameOrAfter(currentDate, 'day');
+  });
+
   return (
     <>
-      {(query as TripDto[]).map(trip => (
-        <div key={trip.tripId}>
-          <h1>{trip.name}</h1>
-          <p>{trip.description}</p>
-        </div>
+      {trips.map(trip => (
+          <CurrentTravelCard
+              description={trip.description}
+              key={trip.tripId}
+              title={trip.name}
+              img={"https://media.admagazine.fr/photos/632449f714420dbf05ebc6f9/16:9/w_2560%2Cc_limit/GettyImages-627393180.jpg"}
+              startDate={trip.startDate}
+              id={trip.tripId}
+          />
+
       ))}
     </>
   );
