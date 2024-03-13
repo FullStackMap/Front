2a66bc63646c4c76a7ab8d2175e2ce4c36b3ea4c
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2024-03-13 20:29:37 +0100

    Features React Query add - '@tanstack/react-query' - '@tanstack/react-query-devtools' init on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { createTheme, MantineProvider } from '@mantine/core';
-import '@mantine/core/styles.css';
-import { Notifications } from '@mantine/notifications';
-import '@mantine/notifications/styles.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import UseScrollTop from './hooks/useScrollTop.tsx';
+import { createTheme, MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
+import { Notifications } from '@mantine/notifications'
+import '@mantine/notifications/styles.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import UseScrollTop from './hooks/useScrollTop.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const theme = createTheme({
   fontFamily: 'Poppins, sans-serif',
@@ -24,10 +26,19 @@ const theme = createTheme({
     lg: '74em',
     xl: '90em',
   },
-});
+})
 
-const rootElement = document.getElementById('root')!;
-const root = ReactDOM.createRoot(rootElement);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 30,
+    },
+  },
+})
+
+const rootElement = document.getElementById('root')!
+const root = ReactDOM.createRoot(rootElement)
 
 root.render(
   <React.StrictMode>
@@ -35,8 +46,11 @@ root.render(
       <UseScrollTop />
       <MantineProvider defaultColorScheme="auto" theme={theme}>
         <Notifications limit={5} position="top-right" />
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>,
-);
+)
