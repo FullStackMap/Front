bf247c5277f2f70bb52bf789f0d82e533aed79fa
Dercraker <antoine.capitain@gmail.com>
2024-03-17 02:02:49 +0100

    Feat (map) : [WIP] Ajout d'un trajet
    - Encore des erreurs de types

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,14 +1,24 @@
 import { useDocumentTitle } from '@mantine/hooks';
 import { IconPinnedFilled } from '@tabler/icons-react';
+import type { FeatureCollection } from 'geojson';
 import React, { useMemo } from 'react';
-import { GeolocateControl, Map, Marker, ScaleControl } from 'react-map-gl';
+import type { CircleLayer } from 'react-map-gl';
+import {
+  GeolocateControl,
+  Layer,
+  Map,
+  Marker,
+  ScaleControl,
+  Source,
+} from 'react-map-gl';
+
 const TestPage = () => {
   useDocumentTitle('From A2B - Test');
 
   const [viewState, setViewState] = React.useState({
-    latitude: 45.73050608112574,
     longitude: 4.860200783597507,
-    zoom: 22,
+    latitude: 45.73050608112574,
+    zoom: 8,
     // zoom: 2,
     pitch: 0,
     // pitch: 30,
@@ -18,16 +28,20 @@ const TestPage = () => {
   const steps = useMemo(
     () => [
       {
-        latitude: 45.73050608112574,
         longitude: 4.860200783597507,
+        latitude: 45.73050608112574,
       },
       {
-        latitude: 51.5073219,
+        longitude: 5.023607756386476,
+        latitude: 45.68705246320726,
+      },
+      {
         longitude: -0.1276474,
+        latitude: 51.5073219,
       },
       {
-        latitude: 40.712776,
         longitude: -74.005974,
+        latitude: 40.712776,
       },
     ],
     [],
@@ -49,6 +63,46 @@ const TestPage = () => {
     });
   }, [steps]);
 
+  // const [geo, setGeo] = React.useState({});
+
+  // async function getRoute() {
+  //   const query = await fetch(
+  //     `https://api.mapbox.com/directions/v5/mapbox/driving/4.860200783597507,45.73050608112574;5.023607756386476,45.68705246320726/?access_token=pk.eyJ1IjoiZGVyY3Jha2VyIiwiYSI6ImNsdHVnczc4dTB6N2QyanFwZDR1N2c2eHoifQ.arP7tBErlINY3-uiwfb7Ww`,
+  //     { method: 'GET' },
+  //   );
+  //   const json = await query.json();
+  //   const data = json.routes[0];
+  //   const route = data.geometry.coordinates;
+  //   const geojson = {
+  //     type: 'Feature',
+  //     properties: {},
+  //     geometry: {
+  //       type: 'LineString',
+  //       coordinates: route,
+  //     },
+  //   };
+  //   setGeo(geojson);
+  // }
+
+  const geojson: FeatureCollection = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: { type: 'Point', coordinates: [-122.4, 37.8] },
+      },
+    ],
+  };
+
+  const layerStyle: CircleLayer = {
+    id: 'point',
+    type: 'circle',
+    paint: {
+      'circle-radius': 10,
+      'circle-color': '#007cbf',
+    },
+  };
+
   return (
     <>
       <Map
@@ -62,10 +116,34 @@ const TestPage = () => {
         style={{
           height: '80vh',
           width: '100vw',
-        }}>
+        }}
+        // onLoad={() => getRoute}
+      >
         <GeolocateControl />
         <ScaleControl />
         {pins}
+        <Source id="my-data" type="geojson" data={geojson}>
+          <Layer {...layerStyle} />
+        </Source>
+        {/* <Source id="route" type="geojson" data={geo}>
+          <Layer
+            id="route"
+            type="line"
+            source={{
+              type: 'geojson',
+              data: geo,
+            }}
+            layout={{
+              'line-join': 'round',
+              'line-cap': 'round',
+            }}
+            paint={{
+              'line-color': '#3887be',
+              'line-width': 5,
+              'line-opacity': 0.75,
+            }}
+          />
+        </Source> */}
       </Map>
     </>
   );
