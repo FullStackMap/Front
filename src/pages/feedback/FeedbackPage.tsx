2d86d13dce557ec5bec3f4382f98f540c2338e70
Dercraker <antoine.capitain@gmail.com>
2024-03-26 22:59:07 +0100

    Fix (FeedBack) : Errors Fixes

--- src/pages/feedback/FeedbackPage.tsx
+++ src/pages/feedback/FeedbackPage.tsx
@@ -1,51 +1,57 @@
-import { useRef } from 'react';
-import {
-  Container,
-  Title,
-  Text,
-  Group,
-  Rating,
-  Center,
-  Card,
-  Badge,
-  Loader,
-} from '@mantine/core';
+import { TestimonialDto } from '@FullStackMap/from-a2b';
 import { Carousel } from '@mantine/carousel';
-import Autoplay from 'embla-carousel-autoplay';
-import { FormFeedback } from '../../components/feedback/FormFeedback';
-import { AnoTestimonialsController } from '../../services/BaseApi';
+import {
+  Badge,
+  Card,
+  Center,
+  Container,
+  Group,
+  Loader,
+  Rating,
+  Text,
+  Title,
+} from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
+import Autoplay from 'embla-carousel-autoplay';
+import { useEffect, useRef, useState } from 'react';
+import { FormFeedback } from '../../components/feedback/FormFeedback';
 import useNotify, { NotifyDto } from '../../hooks/useNotify';
+import { AnoTestimonialsController } from '../../services/BaseApi';
 
 const FeedbackPage = () => {
   const autoplay = useRef(Autoplay({ delay: 5000 }));
   const { ErrorNotify } = useNotify();
 
-  const { isPending, isError, data, error } = useQuery({
-    queryKey: ['testimonials'],
-    queryFn: AnoTestimonialsController.getAllTestimonialGET,
+  const {
+    isPending,
+    isError,
+    data: reviews,
+  } = useQuery({
+    queryKey: ['Testimonials'],
+    queryFn: async () =>
+      await AnoTestimonialsController.getAllTestimonialGET()
+        .then(res => res.data)
+        .catch(() => {
+          ErrorNotify({
+            title: 'Erreur',
+            message: 'Impossible de récupérer les avis',
+          } as NotifyDto);
+        }),
   });
 
-  if (error) {
-    ErrorNotify({
-      title: 'Erreur',
-      message: 'Impossible de récupérer les avis',
-    } as NotifyDto);
-  }
+  const [averageRating, setAverageRating] = useState(0);
 
-  const reviews = data;
-  let averageRating = 0;
+  useEffect(() => {
+    if (!reviews) return;
 
-  if (reviews?.data) {
-    const validReviews = reviews.data.filter(
-      (review) => typeof review.rate === 'number' && review.rate !== undefined
-    );
-    if (validReviews.length > 0) {
-      averageRating =
-        validReviews.reduce((acc, review) => acc + review.rate!, 0) /
-        validReviews.length;
-    }
-  }
+    const avgRating: number =
+      reviews.reduce(
+        (acc: number, review: TestimonialDto) => acc + review.rate!,
+        0,
+      ) / reviews.length;
+
+    setAverageRating(avgRating);
+  }, [reviews]);
 
   return (
     <Container size="md">
@@ -61,12 +67,14 @@ const FeedbackPage = () => {
         </Center>
       )}
       {isError && <Text ta="center">Une erreur est survenue</Text>}
-      {reviews && reviews.data.length === 0 && (
-        <Text ta="center" pt="lg">
-          Soyez le premier à donner votre avis !
-        </Text>
-      )}
-      {!isPending && reviews && reviews.data.length > 0 && (
+      {reviews && reviews.length === 0 ? (
+        <>
+          <Text ta="center" pt="lg">
+            Soyez le premier à donner votre avis !
+          </Text>
+        </>
+      ) : null}
+      {!isPending && reviews && reviews.length > 0 ? (
         <Carousel
           slideSize="70%"
           plugins={[autoplay.current]}
@@ -77,7 +85,7 @@ const FeedbackPage = () => {
           loop
           mt="md"
           mb="md">
-          {reviews?.data.map((review: any) => (
+          {reviews?.map((review: any) => (
             <Carousel.Slide key={review.userId}>
               <Card shadow="sm" radius="md" withBorder h={190}>
                 <Card.Section mah="70%">
@@ -103,7 +111,7 @@ const FeedbackPage = () => {
             </Carousel.Slide>
           ))}
         </Carousel>
-      )}
+      ) : null}
       <Title order={3} mt="xl" ta="center">
         La note attribuée par nos clients
       </Title>
