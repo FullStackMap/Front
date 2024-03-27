d3e4e1dc4fc5d7f03b988c3a7d41aaa602a0a599
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2024-03-28 00:58:10 +0100

    remove editTravel.tsx page

--- src/pages/showTravels/editTravel/editTravel.tsx
+++
@@ -1,9 +0,0 @@
-import { MapStepEditor } from '../../../components/stepEditor/MapStepEditor.tsx'
-export function EditTravel() {
-
-  return (
-    <div>
-      <MapStepEditor></MapStepEditor>
-    </div>
-  );
-}
